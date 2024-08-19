5d796c8053e2c4e79e16aa02451035bdc4660949
Matt Barker <mattbarker23@hotmail.com>
2024-08-19 22:40:55 +0100

    amended images to avoid getting sued (slim chance but still a chance

--- db/data/baskets.js
+++ db/data/baskets.js
@@ -4,19 +4,4 @@ module.exports = [
     product_id: null,
     quantity: 0,
   },
-  {
-    user_id: 1,
-    product_id: 1,
-    quantity: 2,
-  },
-  {
-    user_id: 1,
-    product_id: 2,
-    quantity: 3,
-  },
-  {
-    user_id: 1,
-    product_id: 3,
-    quantity: 4,
-  },
 ];
